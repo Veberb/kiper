2dd11b0ae74cdad94f4c1b18eecb4e96cc4313ee
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-06 23:58:18 -0300

    Creating apartment page

--- packages/client/src/pages/Apartment/index.js
+++ packages/client/src/pages/Apartment/index.js
@@ -1,13 +1,80 @@
-import React from 'react'
-//import './styles.css'
+import React from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { Formik } from 'formik';
+import { useMutation } from '@apollo/react-hooks';
+
+import { CreateApartmentSchema } from '../../validation';
+import { CREATE_APARTMENT } from '../../mutation';
 
 export default function Apartment() {
-    return (
-        <React.Fragment>
-            <div>
-            </div>
+  const [createApartment, { data }] = useMutation(CREATE_APARTMENT);
 
-        </React.Fragment>
-
-    )
+  return (
+    <React.Fragment>
+      <Container>
+        <div>
+          <h1>Cadastro Apartamento</h1>
+          <Formik
+            initialValues={{ name: '', number: 0, block: '' }}
+            validationSchema={CreateApartmentSchema}
+            onSubmit={(values, actions) => {
+              createApartment({ variables: values });
+              /*setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                console.log(values);
+                actions.setSubmitting(false);
+              }, 1000);*/
+            }}
+          >
+            {({ handleSubmit, handleChange, values, errors, touched }) => (
+              <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Nome do apartamento"
+                    onChange={handleChange}
+                    value={values.name}
+                    name="name"
+                  />
+                  {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                </Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>Número</Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Número"
+                        onChange={handleChange}
+                        value={values.number}
+                        name="number"
+                      />
+                      {errors.number && touched.number ? <div>{errors.number}</div> : null}
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>Bloco</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Bloco"
+                        onChange={handleChange}
+                        value={values.block}
+                        name="block"
+                      />
+                      {errors.block && touched.block ? <div>{errors.block}</div> : null}
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </Container>
+    </React.Fragment>
+  );
 }
