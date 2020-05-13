6dd469b6837f09ed41390d59eb286862f0dd83cc
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-13 00:05:43 -0300

    Modal alerting residents will get deleted

---
+++ packages/client/src/components/modal/Apartment/DeleteApartmentModal.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import { Modal, Button } from 'react-bootstrap';
+
+const DeleteApartmentModal = () => {
+  return (
+    <>
+      <div>Ao excluir um apartamento todos seus moradores também serão excluidos. Deseja continuar?</div>
+    </>
+  );
+};
+
+export default DeleteApartmentModal;
