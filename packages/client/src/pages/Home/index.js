8993f21623a5713cdc14ec2ef89f7bfd0ba0c021
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-13 10:51:22 -0300

    button to add apartment

--- packages/client/src/pages/Home/index.js
+++ packages/client/src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React, { useState, use } from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table, Row, Col, Button } from 'react-bootstrap';
 import { PencilSquare, Trash } from 'react-bootstrap-icons';
+import { useHistory } from 'react-router-dom';
 
 import SearchBar from '../../components/searchBar';
 import Modal from '../../components/modal/';
@@ -13,9 +14,9 @@ export default function Home() {
   const [searchName, setSearchName] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [currentApartment, setCurrentApartment] = useState('');
+  const history = useHistory();
 
   const { loading, error, data } = useQuery(ApartmentQuery.GET_APARTMENTS, { variables: { name: searchName } });
-
   const [deleteApartment] = useMutation(ApartmentMutation.DELETE_APARTMENT, {
     refetchQueries: [{ query: ApartmentQuery.GET_APARTMENTS, variables: { name: searchName } }],
   });
@@ -33,42 +34,54 @@ export default function Home() {
         <Modal show={showModal} title="Atenção" onHide={() => setShowModal(false)} onClick={removeApartment}>
           <DeleteApartmentModal />
         </Modal>
-        <SearchBar searchValue={setSearchName} searchName={searchName} />
-        <Table striped bordered hover size="sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Nome</th>
-              <th>Número</th>
-              <th>Bloco</th>
-              <th>Ação</th>
-            </tr>
-          </thead>
-          <tbody>
-            {!loading &&
-              data.getApartments.map((apartment, index) => (
-                <tr key={index}>
-                  {Object.keys(apartment).map((key, keyIndex) => {
-                    if (!keyIndex) return <td key={keyIndex}>{index + 1}</td>;
-                    return <td key={keyIndex}>{apartment[key]}</td>;
-                  })}
-                  <td className="actions">
-                    <div>
-                      <PencilSquare />
-                    </div>
-                    <div
-                      onClick={() => {
-                        setShowModal(true);
-                        setCurrentApartment(apartment);
-                      }}
-                    >
-                      <Trash />
-                    </div>
-                  </td>
+        <Row>
+          <Col sm={9}>
+            <SearchBar searchValue={setSearchName} searchName={searchName} />
+          </Col>
+
+          <Col sm={2}>
+            <Button onClick={() => history.push('/apartment')}>Cadastrar</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nome</th>
+                  <th>Número</th>
+                  <th>Bloco</th>
+                  <th>Ação</th>
                 </tr>
-              ))}
-          </tbody>
-        </Table>
+              </thead>
+              <tbody>
+                {!loading &&
+                  data.getApartments.map((apartment, index) => (
+                    <tr key={index}>
+                      {Object.keys(apartment).map((key, keyIndex) => {
+                        if (!keyIndex) return <td key={keyIndex}>{index + 1}</td>;
+                        return <td key={keyIndex}>{apartment[key]}</td>;
+                      })}
+                      <td className="actions">
+                        <div>
+                          <PencilSquare />
+                        </div>
+                        <div
+                          onClick={() => {
+                            setShowModal(true);
+                            setCurrentApartment(apartment);
+                          }}
+                        >
+                          <Trash />
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
       </Container>
     </React.Fragment>
   );
