51ece0dcfac6a378b2f6fad43bc442eb0be0c6cf
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-13 00:05:50 -0300

    Modal component

---
+++ packages/client/src/components/modal/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+import { Modal, Button } from 'react-bootstrap';
+
+const ModalComponent = ({ title, show, onHide, onClick, children, cancel = 'Cancelar', submit = 'Submit' }) => {
+  return (
+    <>
+      <Modal show={show} onHide={onHide} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>
+            {cancel}
+          </Button>
+          <Button variant="primary" onClick={onClick}>
+            {submit}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComponent;
