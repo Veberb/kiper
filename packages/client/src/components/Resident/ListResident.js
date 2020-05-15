8259f3d7413827ccc3d4b5c32b67755248752b4f
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-15 17:36:10 -0300

    Creating lisResidents by Apartment component (not complete)

---
+++ packages/client/src/components/Resident/ListResident.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import { Table } from 'react-bootstrap';
+import { PencilSquare, Trash } from 'react-bootstrap-icons';
+
+const ListResident = ({ listResidents = [] }) => {
+  return (
+    <>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Número</th>
+            <th>Bloco</th>
+            <th>Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listResidents.map((resident, index) => (
+            <tr key={index}>
+              {Object.keys(resident).map((key, keyIndex) => {
+                if (!keyIndex) return <td key={keyIndex}>{index + 1}</td>;
+                return <td key={keyIndex}>{resident[key]}</td>;
+              })}
+              <td className="actions">
+                <div onClick={() => {}}>
+                  <PencilSquare />
+                </div>
+                <div onClick={() => {}}>
+                  <Trash />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      ;
+    </>
+  );
+};
+
+export default ListResident;
