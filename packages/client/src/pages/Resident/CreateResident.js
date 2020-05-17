86860fd77f8dda9a2ed24dbe47f04fa118ddf4e8
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-17 14:17:54 -0300

    Page to create Resident

---
+++ packages/client/src/pages/Resident/CreateResident.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import { useMutation } from '@apollo/react-hooks';
+import { useHistory, useParams } from 'react-router-dom';
+
+import { ResidentMutation } from '../../services/apollo';
+import ResidentForm from '../../components/Resident/ResidentForm';
+
+export default function CreateResident() {
+  const { id } = useParams();
+  const [createResident] = useMutation(ResidentMutation.CREATE_RESIDENT);
+  const history = useHistory();
+
+  const formik = useFormik({
+    initialValues: {
+      name: 'Lucas',
+      birth: '',
+      phone: '4899640897',
+      cpf: '09152508960',
+      email: 'lucas@conpas.il.com',
+      responsible: false,
+      apartment: '',
+    },
+
+    onSubmit: async (values) => {
+      values.apartment = id;
+      await createResident({ variables: { resident: values } });
+      history.push(`/apartment/${id}/resident/`);
+    },
+  });
+
+  return (
+    <React.Fragment>
+      <Container>
+        <div>
+          <h1>Cadastrar Morador</h1>
+        </div>
+        <ResidentForm formik={formik} />
+      </Container>
+    </React.Fragment>
+  );
+}
