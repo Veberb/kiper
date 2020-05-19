bbef020b44c8b8d5520a6723098b97e9cbdd9298
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-18 23:33:17 -0300

    creating userForm

---
+++ packages/client/src/components/User/UserForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const UserForm = ({ formik }) => {
+  return (
+    <>
+      <Form.Group>
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          name="email"
+          isInvalid={!!formik.errors.email}
+        />
+        <Form.Control.Feedback type="invalid">{formik.errors.email}</Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          name="password"
+          isInvalid={!!formik.errors.password}
+        />
+        <Form.Control.Feedback type="invalid">{formik.errors.password}</Form.Control.Feedback>
+      </Form.Group>
+    </>
+  );
+};
+export default UserForm;
