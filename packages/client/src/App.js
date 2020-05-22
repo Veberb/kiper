cdf659283f80ceabd3a2380c0b12546cf32e60ed
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-22 11:04:18 -0300

    Adding breadcrumb to header and some css

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './App.css';
 import { ToastProvider } from 'react-toast-notifications';
-
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Breadcrumb from './components/Breadcrumb';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
       <div className="card">
-        <ToastProvider autoDismiss autoDismissTimeout={3500}>
-          <Routes />
-        </ToastProvider>
+        <BrowserRouter>
+          <ToastProvider autoDismiss autoDismissTimeout={3500}>
+            <Container>
+              <Row className="rowMargin">
+                <Col sm={8}>
+                  <Breadcrumb></Breadcrumb>
+                </Col>
+                <Col sm={2}>
+                  <Button variant="primary">Sair</Button>
+                </Col>
+              </Row>
+            </Container>
+            <Routes />
+          </ToastProvider>
+        </BrowserRouter>
       </div>
     </div>
   );
