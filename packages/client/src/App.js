5630cf8546386e90881187bb6a7f011546289c65
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-22 11:37:22 -0300

    Logout function  in header

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,9 +4,14 @@ import { ToastProvider } from 'react-toast-notifications';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Breadcrumb from './components/Breadcrumb';
 import { BrowserRouter } from 'react-router-dom';
+
 import Routes from './routes';
 
 function App() {
+  const logout = () => {
+    window.location.pathname = '/';
+  };
+
   return (
     <div className="App">
       <div className="card">
@@ -14,11 +19,13 @@ function App() {
           <ToastProvider autoDismiss autoDismissTimeout={3500}>
             <Container>
               <Row className="rowMargin">
-                <Col sm={8}>
+                <Col sm={10}>
                   <Breadcrumb></Breadcrumb>
                 </Col>
                 <Col sm={2}>
-                  <Button variant="primary">Sair</Button>
+                  <Button variant="primary" onClick={() => logout()}>
+                    Sair
+                  </Button>
                 </Col>
               </Row>
             </Container>
