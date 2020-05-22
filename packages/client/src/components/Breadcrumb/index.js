d07b03e75fea5ba44c8e50fefa7087ff614d9e08
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-05-22 11:04:02 -0300

    Breadcrum component

---
+++ packages/client/src/components/Breadcrumb/index.js
@@ -0,0 +1,23 @@
+import React, { useMemo } from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import useBreadcrumb from '../../utils/hooks/useBreadcrumb';
+
+const Wrap = ({ to, ...props }) => (to ? <Link {...props} to={to} /> : <React.Fragment {...props} />);
+
+export default function BreadcrumbHeader() {
+  const { current } = useBreadcrumb();
+
+  return (
+    <>
+      <Breadcrumb>
+        {!!current.length &&
+          current.map(({ title, to }, index, arr) => (
+            <BreadcrumbItem key={index} active={arr.length === index + 1}>
+              <Wrap to={to}>{title}</Wrap>
+            </BreadcrumbItem>
+          ))}
+      </Breadcrumb>
+    </>
+  );
+}
